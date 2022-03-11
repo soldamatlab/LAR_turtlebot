6b3a4c1d31cadba9bbb987700d64fd2adc820494
Sheld5 <simonsoldat@seznam.cz>
2022-03-11 11:15:30 +0100

    init basic project structure

---
+++ main.py
@@ -0,0 +1,14 @@
+from robolab_turtlebot import Turtlebot, Rate
+
+turtle = Turtlebot()
+
+def main():
+    rate = Rate(10)
+    while not turtle.is_shutting_down():
+        rate.sleep()
+
+if __name__ == '__main__':
+    img = turtle.get_rgb_image()
+    print(img)
+
+    main()
