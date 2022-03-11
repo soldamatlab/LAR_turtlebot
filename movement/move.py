584299caaacc28c656a4c3a8af759c63da75a74c
soldamat <soldamat@fel.cvut.cz>
2022-03-11 12:17:14 +0100

    add odometry

--- movement/move.py
+++ movement/move.py
@@ -4,23 +4,29 @@ from robolab_turtlebot import Turtlebot, Rate, get_time
 
 from dance import dance
 
-turtle = Turtlebot()
-
 # Names bumpers and events
 bumper_names = ['LEFT', 'CENTER', 'RIGHT']
 state_names = ['RELEASED', 'PRESSED']
 
+def button_cb(msg):
+    dance()
+
 def bumper_cb(msg):
-    # Print the event
     bumper = bumper_names[msg.bumper]
     state = state_names[msg.state]
     print('{} bumper {}'.format(bumper, state))
 
     if state_names[msg.state] == 'PRESSED':
-        dance()
+        odometry = turtle.get_odometry()
+        print('odometry: {}'.format(odometry))
 
+turtle = Turtlebot()
 def main():
+    turtle.register_button_event_cb(button_cb)
     turtle.register_bumper_event_cb(bumper_cb)
+
+    turtle.wait_for_odometry()
+    turtle.reset_odometry()
 
     rate = Rate(10)
     while not turtle.is_shutting_down():
