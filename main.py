10a3f21ac5a691f8d98c7298709ae1b29a9045b1
Sheld5 <simonsoldat@seznam.cz>
2022-03-11 12:46:03 +0100

    wip

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 from robolab_turtlebot import Turtlebot, Rate
 import numpy as np
 import cv2
-from scipy.misc import imsave
 
 turtle = Turtlebot(rgb=True)
 
@@ -15,7 +14,8 @@ if __name__ == '__main__':
     turtle.wait_for_rgb_image()
     rgb = turtle.get_rgb_image()
     hsv = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
-    imsave("rgb.png", rgb)
-    imsave("hsv.png", hsv)
+    window = "view"
+    cv2.namedWindow(window)
+    cv2.imshow(window, rgb)
 
     main()
