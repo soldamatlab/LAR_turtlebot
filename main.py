6a94d319099ffc418e0d82d52daf2646b92702f8
Soldat <soldasim@fel.cvut.cz>
2022-03-11 13:28:34 +0100

    wip

--- main.py
+++ main.py
@@ -7,7 +7,18 @@ from camera_controller import Window
 turtle = Turtlebot(rgb=True, pc=True, depth=True)
 
 
+def button_cb(msg):
+    print('button cb')
+    if msg.state == 0:
+        rgb = turtle.get_rgb_image()
+        hsv = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
+        shape = hsv.shape
+        print(hsv[Int(shape[0]/2),Int(shape[1]/2),:])
+
+
 def main():
+    turtle.register_button_event_cb(button_cb)
+
     rate = Rate(10)
     window = Window("view")
 
