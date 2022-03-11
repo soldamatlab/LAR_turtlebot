2b42bf1ac7549b41b8e2105f3d3a500219a86a51
soldamat <soldamat@fel.cvut.cz>
2022-03-11 10:52:01 +0100

    change rate

--- mata_script.py
+++ mata_script.py
@@ -3,13 +3,13 @@ from __future__ import print_function
 from robolab_turtlebot import Turtlebot, Rate, get_time
 
 turtle = Turtlebot()
-rate = Rate(10)
 
 # Names bumpers and events
 bumper_names = ['LEFT', 'CENTER', 'RIGHT']
 state_names = ['RELEASED', 'PRESSED']
 
 def do_for(duration, action):
+    rate = Rate(1)
     t = get_time()
     while get_time() - t < duration:
         action()
@@ -18,30 +18,25 @@ def do_for(duration, action):
 def step():
     turtle.cmd_velocity(linear=0.1, angular=0)
 
-def rot1():
+def rot_left():
     turtle.cmd_velocity(linear=0, angular=1)
 
-def rot2():
+def rot_right():
     turtle.cmd_velocity(linear=0, angular=-1)
 
 def dance():
     do_for(0.2, step)
     do_for(0.2, step)
-    do_for(0.1, rot1)
-    do_for(0.1, rot2)
-    do_for(0.1, rot1)
+    do_for(0.1, rot_left)
+    do_for(0.1, rot_right)
+    do_for(0.1, rot_left)
 
 
 
 def bumper_cb(msg):
-    """Bumber callback."""
-    # msg.bumper stores the id of bumper 0:LEFT, 1:CENTER, 2:RIGHT
-    bumper = bumper_names[msg.bumper]
-
-    # msg.state stores the event 0:RELEASED, 1:PRESSED
-    state = state_names[msg.state]
-
     # Print the event
+    bumper = bumper_names[msg.bumper]
+    state = state_names[msg.state]
     print('{} bumper {}'.format(bumper, state))
 
     if state_names[msg.state] == 'PRESSED':
@@ -49,10 +44,8 @@ def bumper_cb(msg):
 
 
 def main():
-    # Register bumper callback
     turtle.register_bumper_event_cb(bumper_cb)
 
-    # Do something, the program would end otherwise
     rate = Rate(1)
     while not turtle.is_shutting_down():
         rate.sleep()
