4c8bb001934ac9513e77efd414fbf987515d5805
Soldat <soldasim@fel.cvut.cz>
2022-04-01 11:45:57 +0200

    wip

--- camera.py
+++ camera.py
@@ -101,10 +101,7 @@ def hw_ratio_filter(segments, target=1, max_diff=0.2, info=False):
 
 def recalculate_coordinates(point_cloud, K):
     old_point_cloud = np.array(point_cloud)
-
-    print(np.shape(old_point_cloud))
-
-    og_shape = old_point_cloud.shape()
+    og_shape = np.shape(old_point_cloud)
     old_point_cloud.reshape((og_shape[0] * og_shape[1], og_shape[2]))
     new_point_cloud = old_point_cloud * K
     new_point_cloud.reshape(og_shape)
