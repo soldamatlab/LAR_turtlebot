0881cc8bceeccad81ecca01d145ab7dbaa290b12
Soldat <soldasim@fel.cvut.cz>
2022-04-01 18:57:48 +0200

    wip

--- driver.py
+++ driver.py
@@ -7,7 +7,7 @@ def drive(turtle):
     if sticks.count < 2:
         return
 
-    max_sticks = np.argmax(sticks.areas())
+    max_sticks = np.argsort(sticks.areas())
     A = sticks.coors[max_sticks[0]]
     B = sticks.coors[max_sticks[1]]
     print(A)
