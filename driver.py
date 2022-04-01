dbdbde310b4b9dd0711f617e098f3405fff9bd2f
Soldat <soldasim@fel.cvut.cz>
2022-04-01 18:41:19 +0200

    wip

--- driver.py
+++ driver.py
@@ -3,7 +3,7 @@ import CONST
 def drive(turtle):
     sticks = turtle.get_segments(CONST.GREEN)
 
-    print(sticks.params[:,4])
+    print(sticks.params[:][4])
 
     if sticks.count < 2:
         return
