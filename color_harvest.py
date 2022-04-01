1aca564d7b6b1480c8b981abd9a683156db29178
soldamat <soldamat@fel.cvut.cz>
2022-04-01 13:29:20 +0200

    wip

--- color_harvest.py
+++ color_harvest.py
@@ -1,12 +1,10 @@
 from camera import img_threshold
 
 def print_center_color(turtle, sticks):
-    hsv = turtle.get_hsv_image()
-    bin = img_threshold(hsv)
-    print(hsv.shape)
-    print(bin.shape)
-
     if sticks.count > 0:
+        hsv = turtle.get_hsv_image()
+        bin = img_threshold(hsv)
+
         params = sticks.params[0]
         centroid = [int(sticks.centroids[0][0]), int(sticks.centroids[0][1])]
 
@@ -14,7 +12,7 @@ def print_center_color(turtle, sticks):
         #center_color = hsv[center_coords[0], center_coords[1]]
         #center_bool = bin[center_coords[0], center_coords[1]]
 
-        center_color = hsv[centroid[0], centroid[1]]
-        center_bool = bin[centroid[0], centroid[1]]
+        center_color = hsv[centroid[1], centroid[0]]
+        center_bool = bin[centroid[1], centroid[0]]
 
         print("Centroid 0 coords: " + str(centroid) + ", hsv color: " + str(center_color) + ", bool: " + str(center_bool))
