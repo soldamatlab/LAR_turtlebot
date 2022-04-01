da2e26c228eb60a726808f940cae38ab762125ac
Soldat <soldasim@fel.cvut.cz>
2022-04-01 14:57:35 +0200

    wip

--- camera.py
+++ camera.py
@@ -129,8 +129,7 @@ def hw_ratio_filter(segments, target=1, max_diff=0.2, info=False):
     return segments
 
 
-# TODO lambda (l)
-def pc_cam_to_bot(point_cloud, K, l=1):
+def pc_cam_to_bot(point_cloud, K, l=CONST.CAM_LAMBDA):
     cam_pc = np.array(point_cloud)
     og_shape = np.shape(cam_pc)
     cam_pc = np.reshape(cam_pc, (og_shape[0] * og_shape[1], og_shape[2]))
