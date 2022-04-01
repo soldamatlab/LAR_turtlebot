6502075d07c8793b8ba657e497430fe6775a3876
Soldat <soldasim@fel.cvut.cz>
2022-04-01 15:07:15 +0200

    wip

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def button_1():
 
 def button_2():
     arr = []
-    arr.append(np.array([0,0,0])
+    arr.append(np.array([0,0,0]))
     arr.append(np.array([1,2,3]))
     arr.append(np.array([5,5,5]))
     print(np.median(arr, axis=0))
