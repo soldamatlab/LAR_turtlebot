3eaecc26b26fc7cffd6e08b72acba9658da12236
soldamat <soldamat@fel.cvut.cz>
2022-04-01 17:17:42 +0200

    wip

--- camera.py
+++ camera.py
@@ -64,7 +64,9 @@ def img_threshold(hsv, color):
 
 def get_hue_diff(hue_array, target_hue):
     diff1 = np.abs(hue_array - target_hue)
+    print("DIFF1: " + str(diff1))
     diff2 = np.abs(hue_array - (target_hue - CONST.HUE_MAX))
+    print("DIFF2: " + str(diff2))
     return np.minimum(diff1, diff2)
 
 
