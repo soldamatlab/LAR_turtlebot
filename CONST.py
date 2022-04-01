38f11cc5a29af9eeef053be670d90c7a007c125b
soldamat <soldamat@fel.cvut.cz>
2022-04-01 14:49:40 +0200

    wip

--- CONST.py
+++ CONST.py
@@ -1,11 +1,13 @@
 
 # POLE COLOR
-GREEN = 65
+GREEN = 65  # 45 - 75
+BLUE = 100  # 95 - 
+RED = 5     #  0 - 
 HUE_DIFF = 25
-SATUR_MIN = 50 #100
-VAL_MIN = 20 #70
+SATUR_MIN = 50
+VAL_MIN = 20    
 
 # POLE SEGMENT
 MIN_AREA = 1000
-TARGET_RATIO = 4.9 # ~6 when truly in center, ~3.5 when on the far side
-MAX_RATIO_DIFF = 1.5
+TARGET_RATIO = 5.35 # ~6 when truly in center, ~3.5 when on the far side
+MAX_RATIO_DIFF = 1.85
