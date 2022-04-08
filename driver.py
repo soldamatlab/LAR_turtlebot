48d1b19d31ba2b95a5f7a6047b1816f8fe68a964
Sheld5 <simonsoldat@seznam.cz>
2022-04-08 12:51:46 +0200

    wip

--- driver.py
+++ driver.py
@@ -112,7 +112,6 @@ class FindTwoSticks(Activity):
             self.w_bin.show(bin_to_rgb(bin_img))
 
         if sticks.count < 2:
-            print("FIND")
             self.turtle.set_speed(0, self.speed)
             return
 
@@ -123,18 +122,18 @@ class FindTwoSticks(Activity):
 
         # TODO
         stick_mean = sticks.coors[0]
+        print(stick_mean)
+        print(stick_mean[0])
+        quit()
 
         if self.center:
             if not self.centered(stick_mean):
                 if stick_mean[0] < 0:
-                    print("LEFT")
                     self.turtle.set_speed(0, self.speed)
                 else:
-                    print("RIGHT")
                     self.turtle.set_speed(0, -self.speed)
                 return
 
-        print("END")
         self.parent.ret = stick_mean
         self.turtle.stop()
         self.end()
