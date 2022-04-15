e06a101aba7ca04fe87337cccf72cf64c55a5c95
Soldat <soldasim@fel.cvut.cz>
2022-04-15 15:23:06 +0200

    wip

--- driver.py
+++ driver.py
@@ -237,6 +237,7 @@ class GoThroughGate(Activity):
             midturn_point = self.calculate_first_step(A, B, gate_center, self.turn_offset)
             self.second_step = self.calculate_second_step(midturn_point, gate_center)
             print("------------------------ DEUBG")  # TODO rem
+            print("A: " + str(A) + " B: " + str(B))
             print("first_step: " + str(midturn_point))
             print("second_step: " + str(self.second_step))
             self.step = 1
