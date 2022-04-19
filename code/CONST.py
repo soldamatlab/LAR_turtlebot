b7bc89939c41615022e7853332abe69ffef56415
Sheld5 <simonsoldat@seznam.cz>
2022-04-19 16:24:07 +0200

    wip

--- code/CONST.py
+++ code/CONST.py
@@ -34,7 +34,7 @@ TARGET_RATIO = 5.1  # 5.35  # ~6 when truly in center, ~3.5 when on the far side
 MAX_RATIO_DIFF = 3  # 1.85
 
 # CAM
-DEPTH_CAM_LAMBDA = 429.363066725704
+DEPTH_CAM_LAMBDA = 20000 #429.363066725704 TODO !!!
 
 # UTILS
 def get_color_consts(color):
