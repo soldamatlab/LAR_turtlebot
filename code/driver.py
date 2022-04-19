e9dc43b21e85b8343126a676a2c760d55c78914c
Sheld5 <simonsoldat@seznam.cz>
2022-04-19 16:21:41 +0200

    wip

--- code/driver.py
+++ code/driver.py
@@ -523,7 +523,7 @@ class MeasureGateCoordinates(Activity):
             self.attempts -= 1
             return self.perform()
 
-        pc = self.turtle.get_point_cloud(convert_to_bot=True)  # TODO
+        pc = self.turtle.get_point_cloud(convert_to_bot=False)  # TODO
         sticks.calculate_coors(pc)
 
         args = np.argsort(sticks.areas())
