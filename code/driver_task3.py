7437ee468cb47e1f95f328ff393f4f41aa37fc01
Sheld5 <simonsoldat@seznam.cz>
2022-04-20 15:52:41 +0200

    wip

--- code/driver_task3.py
+++ code/driver_task3.py
@@ -110,13 +110,7 @@ class ThirdTask(Activity):
             return self.activity.perform()
 
         if self.activity is None:
-            return self.do(MoveStraight(self, self.driver, 0.1))
-
-        if isinstance(self.activity, MoveStraight):
-            return self.do(Turn(self, self.driver, np.pi/2))
-
-        if isinstance(self.activity, Turn):
-            return self.do(GotoCoors(self, self.driver, [-0.1, 0.1]))
+            return self.do(GotoCoors(self, self.driver, [0.1,0.1]))
 
         return self.end()
 
