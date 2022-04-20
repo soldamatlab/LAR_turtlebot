053b1c9451c7965e5c9249b7249ca98d09d52f70
Sheld5 <simonsoldat@seznam.cz>
2022-04-20 20:08:33 +0200

    wip

--- code/main.py
+++ code/main.py
@@ -1,11 +1,6 @@
 from __future__ import print_function
 from robolab_turtlebot import Rate
 from turtle import Turtle
-import numpy as np
-import cv2
-from camera import *
-from dance import dance
-from color_harvest import *
 from driver_task3 import Driver
 import CONST
 
